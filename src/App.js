9d9fe2583b9114f6319e158738420f546190e9b0
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-04 02:49:44 +0530

    this is my dummy 2 commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useState} from "react";
 
 function App() {
+  const [text,setText]=useState('');
+  //1. approach-> every renderuseEffect(()=>{
+    //console.log("ui rendering is done");
+
+  //});// you write the useEffect hook in the component in that component when render happen of that component the code run inside the useEffect hook, in which component you use seEffect apart fro that component in any other component when any change happen it is handled by the useEffect hook
+  //2. approach -> on only single render first render
+  useEffect(()=>{
+    console.log("ui rendering is doone");
+
+  },[]);// second parameter in useeffect basically show the dependencies
+  function changeHandler(event){
+    console.log(text);
+    setText(event.target.value);
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={changeHandler}></input>
     </div>
   );
 }
